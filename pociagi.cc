39a2c2e745bc85550bcd5b31e04121f556a41597
Tommalla <tommalla666@gmail.com>
2013-10-09 21:08:16 +0200

    Dodano maina

---
+++ pociagi.cc
@@ -0,0 +1,9 @@
+/* Tomasz Zakrzewski i Maciej Dmowski /
+/  JNP 2013/2014, Projekt 1 */
+#include <cstdio>
+
+//TODO: wczytywanie, parsowanie, walidacja, drzewo przedziałowe, tablice prefiksowe, algorytm
+
+int main() {
+	return 0;
+}
