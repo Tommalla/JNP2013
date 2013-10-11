077435c6925c4e5cf689a3abd2eb9a113bfabdf8
Maciej Dmowski <M.Dmowski@mimuw.edu.pl>
2013-10-11 23:18:29 +0200

    pierwsze algorytmy

--- pociagi.cc
+++ pociagi.cc
@@ -11,6 +11,8 @@ using namespace std;
 
 typedef unsigned short int Delay;	//typedef, bo można łatwo zmodyfikować
 typedef unsigned short int Hour;
+typedef unsigned short int TrainNo;
+
 const Delay MAX_DELAY = USHRT_MAX;	//do walidacji opóźnienia
 
 //cała impelementacja drzewa przedziałowego zaczyna się tu
@@ -20,16 +22,74 @@ inline void insert(Hour h, const Delay &d) {
 	//TODO operacja insert w drzewie
 }
 
-inline const Delay& query(Hour b, Hour e) {
+//tutaj konczy sie implementacja drzewa przedziałowego;
+
+TrainNo l[DAYSIZE];
+Delay s[DAYSIZE];
+
+inline const void addTrainL(const Hour &h,const Delay &d){
+  l[h+d]++; 
+}
+inline const void addTrainS(const Hour &h, const Delay &d){
+  s[h+d]+=d;
+}
+
+inline const void updateData(){
+  // Kiedy skonczymy wczytywać dane o pociagach wywolujemy tę funkcje. 
+  // Ona reorganizuje nam struktury.
+  // Liczy sie tablica prefiksowa l;
+  // Liczy sie drzewko podzialowe tree;
+  // Liczy sie tablica prefiksowa s;
+
+  updateL();
+  updateM();
+  updateS();
+}
+
+
+
+inline const void updateL(){
+  int suma =0;
+  for(int i=0;i<DAYSIZE;i++){
+    suma+=l[i];
+    l[i]=suma;
+  }
+}
+
+inline const void updateM(){
+    //TODO operacja tworzenia drzewa podzialowego na podstawie wczesniej wczytanych danych;
+}
+
+inline const void updateS(){
+  int suma = 0;
+  for (int i=0;i<DAYSIZE;i++){
+    suma+=s[i];
+    s[i]=suma;
+  }
+}
+
+inline const TrainNo& queryL(Hour b, Hour e) {
+	//TODO operacja obliczania liczby pociagów które przejechaly przez posterunek na przedziale [b; e] (ogolna idea: l[e]-l[b-1]);
+        return l[e] - (b>0 ? l[b-1] : 0);
+}
+
+inline const Delay& queryM(Hour b, Hour e) {
 	//TODO operacja obliczania max opóźnienia na przedziale [b; e] (drzewo przedziałowe)
 }
 
-//cała implementacja drzewa przedziałowego kończy się tu
+inline const Delay& queryS(Hour b, Hour e) {
+	//TODO operacja obliczania sumy opóźnień na przedziale [b; e] (Ogólna idea: s[e]-s[b-1]);
+        return s[e] - (b>0 ? s[b-1] : 0);
+
+}
 
 inline void addTrain(const Hour &h, const Delay &d) {
 	//TODO dodawanie pociągu do struktur
 	//w szczególności do drzewa przedziałowego:
+        
+        addTrainL(h,d);
 	insert(h, d);
+        addTrainS(h,d);
 }
 
 inline void printError(const string& line) {
