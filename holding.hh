5490099d59094a5014f8865e7aec073e4f03ed84
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2013-12-02 16:46:24 +0100

    Partial implementation of Group class.

--- holding.hh
+++ holding.hh
@@ -1,9 +1,10 @@
 #ifndef HOLDING_HH
 #define HOLDING_HH
+#include <cstdio>
+#include <iostream>
 
 typedef unsigned int Quantity;
 
-//TODO: everything
 namespace util {
 	template<typename T>
 	constexpr const T& max(const T& a, const T& b) {
@@ -11,37 +12,209 @@ namespace util {
 	}
 
 	template<typename T>
+	constexpr const T safe_add(const T& a, const T& b) {
+		return max(max(a, b), a + b);
+	}
+
+	template<typename T>
 	constexpr const T safe_sub(const T& a, const T& b) {
 		return (a >= b) ? a - b : 0;
 	}
+
+	template<typename T>
+	constexpr const T safe_div(const T& a, const T& b) {
+		return (b == 0) ? 0 : (a / b);
+	}
 }
 
-template<Quantity accNum = 0, Quantity hsNum = 0, Quantity exNum = 0> struct Company {
+template<Quantity accNum = 0u, Quantity hsNum = 0u, Quantity exNum = 0u>
+struct Company {
 	static constexpr auto _accNum = accNum;
 	static constexpr auto _hsNum = hsNum;
 	static constexpr auto _exNum = exNum;
 };
 
 typedef Company<1, 0, 0> Accountancy;
-typedef Company<0, 1, 0> Huntihg_shop;
+typedef Company<0, 1, 0> Hunting_shop;
 typedef Company<0, 0, 1> Exchange_office;
 
-template<class C1, class C2> struct add_comp {
+template<class C1, class C2>
+struct add_comp {
 	typedef Company<C1::_accNum + C2::_accNum, C1::_hsNum + C2::_hsNum, C1::_exNum + C2::_exNum> type;
 };
 
-template<class C1, class C2> struct remove_comp {
+template<class C1, class C2>
+struct remove_comp {
 	typedef Company<util::safe_sub(C1::_accNum, C2::_accNum), util::safe_sub(C1::_hsNum, C2::_hsNum),
 		util::safe_sub(C1::_exNum, C2::_exNum)> type;
 };
 
-template<class C, unsigned int n> struct multiply_comp {
+template<class C, unsigned int n>
+struct multiply_comp {
 	typedef Company<util::max(C::_accNum * n, C::_accNum), util::max(C::_hsNum * n, C::_hsNum),
 		util::max(C::_exNum * n, C::_exNum)> type;
 };
 
-template<class C, unsigned int n> struct split_comp {
+template<class C, unsigned int n>
+struct split_comp {
 	typedef Company<C::_accNum / n, C::_hsNum / n, C::_exNum / n> type;
 };
+
+template<class C>
+struct additive_expand_comp {
+	typedef Company<util::max(C::_accNum + 1, C::_accNum), util::max(C::_hsNum + 1, C::_hsNum),
+		util::max(C::_exNum + 1, C::_exNum)> type;
+};
+
+template<class C>
+struct additive_rollup_comp {
+	typedef Company<util::safe_sub(C::_accNum, 1u), util::safe_sub(C::_hsNum, 1u),
+		util::safe_sub(C::_exNum, 1u)> type;
+};
+
+//TODO: template Group
+template<class Company>
+class Group {
+	public:
+		typedef Company company_type;
+		static constexpr company_type company = Company();
+
+		//constructors
+		Group() : qty(1), acc_val(DEF_ACC_VAL), hs_val(DEF_HS_VAL), exo_val(DEF_EXO_VAL) {}
+
+		Group(unsigned int k) : Group{} {
+			qty = k;
+		}
+
+		Group(Group<Company> const & other) : Group{} {
+			qty = other.qty;
+		}
+
+		//setters
+		void set_acc_val(unsigned int i) {
+			acc_val = i;
+		}
+
+		void set_hs_val(unsigned int i) {
+			hs_val = i;
+		}
+
+		void set_exo_val(unsigned int i) {
+			exo_val = i;
+		}
+
+		//getters
+		unsigned int get_size() const {
+			return qty;
+		}
+
+		unsigned int get_acc_val() const {
+			return acc_val;
+		}
+
+		unsigned int get_hs_val() const {
+			return hs_val;
+		}
+
+		unsigned int get_exo_val() const {
+			return exo_val;
+		}
+
+		unsigned int get_value() const {
+			return qty * (Company::_accNum * acc_val + Company::_hsNum * hs_val
+				+ Company::_exNum * exo_val);
+		}
+
+		//operators:
+		//FIXME try and figure out how to get rid of this copypasta
+		const Group<Company>& operator+= (const Group<Company>& op) {
+			acc_val = weightedAvg(acc_val, op.acc_val, qty, op.qty);
+			hs_val = weightedAvg(hs_val, op.hs_val, qty, op.qty);
+			exo_val = weightedAvg(exo_val, op.exo_val, qty, op.qty);
+
+			qty += op.qty;
+
+			return *this;
+		}
+
+		const Group<Company> operator+ (const Group<Company>& op) {
+			Group<Company> res(*this);
+			res += op;
+			return res;
+		}
+
+ 		const Group<Company>& operator-= (const Group<Company>& op) {
+			acc_val = weightedAvg(acc_val, op.acc_val, qty, op.qty, true);
+			hs_val = weightedAvg(hs_val, op.hs_val, qty, op.qty, true);
+			exo_val = weightedAvg(exo_val, op.exo_val, qty, op.qty, true);
+
+			qty = util::safe_sub(qty, op.qty);
+
+			return *this;
+ 		}
+
+ 		const Group<Company> operator- (const Group<Company>& op) {
+			Group<Company> res(*this);
+			res -= op;
+			return res;
+ 		}
+
+ 		const Group<Company>& operator*= (const Quantity& op) {
+			qty *= op;
+			acc_val = util::safe_div(acc_val, op);
+			hs_val = util::safe_div(hs_val, op);
+			exo_val = util::safe_div(exo_val, op);
+
+			return *this;
+ 		}
+
+ 		const Group<Company> operator* (const Quantity& op) {
+			Group<Company> res(*this);
+			res *= op;
+			return res;
+ 		}
+
+ 		const Group<Company>& operator/= (const Quantity& op) {
+			qty = util::safe_div(qty, op);
+			acc_val *= op;
+			hs_val *= op;
+			exo_val *= op;
+
+			return *this;
+ 		}
+
+ 		const Group<Company> operator/ (const Quantity &op) {
+			Group<Company> res(*this);
+			res /= op;
+			return res;
+ 		}
+
+ 		template<class T>
+ 		friend std::ostream& operator<< (std::ostream& out, const Group<T>& g);
+
+	private:
+		Quantity qty, acc_val, hs_val, exo_val;
+
+		static constexpr Quantity DEF_ACC_VAL = 15;
+		static constexpr Quantity DEF_HS_VAL = 150;
+		static constexpr Quantity DEF_EXO_VAL = 50;
+
+		const Quantity weightedAvg(const Quantity val1, const Quantity val2,
+			const Quantity qty1, const Quantity qty2, const bool sub = false) {
+			if (!sub)
+				return util::safe_div(val1 * qty1 + val2 * qty2, qty1 + qty2);
+			return util::safe_div(util::safe_sub(val1 * qty1, val2 * qty2), util::safe_sub(qty1, qty2));
+		}
+
+};
+
+template<class Company>
+std::ostream& operator<< (std::ostream& out, const Group<Company>& g) {
+	out << "Number of companies: " << g.qty <<"; Value: " << g.get_value() <<
+	"\nAccountancies value: " << g.acc_val << ", Hunting shops value: " << g.hs_val <<
+	", Exchange offices value: " << g.exo_val << "\nAccountancies: " << Company::_accNum <<
+	", Hunting shops: " << Company::_hsNum << ", Exchange offices: " << Company::_exNum << "\n";
+	return out;
+}
 
 #endif
--- private/main.cpp
+++ private/main.cpp
@@ -7,9 +7,9 @@
 
 using std::is_same;
 
-// typedef add_comp<Accountancy, Exchange_office>::type small_company;
-// typedef add_comp<multiply_comp<Hunting_shop, 10>::type, multiply_comp<Exchange_office, 20>::type>::type money;
-// typedef add_comp<multiply_comp<Hunting_shop, 30>::type, multiply_comp<Exchange_office, 10>::type>::type guns;
+typedef add_comp<Accountancy, Exchange_office>::type small_company;
+typedef add_comp<multiply_comp<Hunting_shop, 10>::type, multiply_comp<Exchange_office, 20>::type>::type money;
+typedef add_comp<multiply_comp<Hunting_shop, 30>::type, multiply_comp<Exchange_office, 10>::type>::type guns;
 
 int main() {
 
@@ -20,54 +20,59 @@ int main() {
 	static_assert(is_same<multiply_comp<Company<3, 2, 1>, 2>::type, Company<6, 4, 2>>::value, "multiply_comp doesn't work[1]");
 	//static_assert(!is_same<multiply_comp<Company<3, 2, 1>, -1>::type, Company<-3, -2, -1>>::value, "multiply_comp doesn't work[2]");
 	static_assert(is_same<split_comp<Company<3, 2, 1>, 2>::type, Company<1, 1, 0>>::value, "split_comp doesn't work");
-// 	Group<Accountancy> s1;
-// 	Group<money> s2(20);
-// 	Group<money> s3(20);
-// 	Group<Exchange_office> s4;
-// 	Group<guns> s5(20);
-//
-// 	assert((s2 + s3).get_size() == 40);
-// 	assert((s2 += s2).get_size() == 40);
-// 	assert(s2.get_size() == 40);
-// 	assert((s2 - s3).get_size() == 20);
-// 	assert((s3 - s2).get_size() == 0);
-//
-// 	s3.set_exo_val(25);
-//
-// 	std::cout << s2;
-// 	std::cout << s3;
-// 	std::cout << s2 + s3;
-// 	std::cout << s2 - s3;
-// 	std::cout << s2 * 4;
-// 	std::cout << s2 / 2;
-//
-// 	assert((s2 + s3).get_value() == 139200);
-// 	assert((s2 - s3).get_value() == 60000);
-// 	assert((s2 * 4).get_value() == 97600);
-// 	assert((s2 / 2).get_value() == 100000);
-//
+	static_assert(is_same<additive_expand_comp<Company<3, 2, 1>>::type, Company<4, 3, 2>>::value, "additive_expand_comp doesn't work");
+	static_assert(is_same<additive_rollup_comp<Company<3, 2, 0>>::type, Company<2, 1, 0>>::value, "additive_expand_comp doesn't work");
+ 	Group<Accountancy> s1;
+ 	Group<money> s2(20);
+ 	Group<money> s3(20);
+ 	Group<Exchange_office> s4;
+ 	Group<guns> s5(20);
+	Group<Company<2, 1, 1>> g;
+
+	assert(g.get_value() == 50 + 150 + 30);
+
+ 	assert((s2 + s3).get_size() == 40);
+ 	assert((s2 += s2).get_size() == 40);
+ 	assert(s2.get_size() == 40);
+ 	assert((s2 - s3).get_size() == 20);
+ 	assert((s3 - s2).get_size() == 0);
+
+ 	s3.set_exo_val(25);
+
+	std::cout << s2;
+	std::cout << s3;
+	std::cout << s2 + s3;
+	std::cout << s2 - s3;
+	std::cout << s2 * 4;
+	std::cout << s2 / 2;
+
+ 	assert((s2 + s3).get_value() == 139200);
+ 	assert((s2 - s3).get_value() == 60000);
+ 	assert((s2 * 4).get_value() == 97600);
+ 	assert((s2 / 2).get_value() == 100000);
+	
 // 	assert(s2 > s3);
 // 	assert(s1 != s4);
 // 	assert(s1 == s1);
 //
-// 	assert(typeid(add_comp<Accountancy, Exchange_office>::type) == typeid(small_company));
-// 	assert(typeid(remove_comp<small_company, Accountancy>::type) == typeid(Exchange_office));
-// 	assert(typeid(split_comp<small_company, 2>::type) == typeid(Company<0, 0, 0>));
-// 	assert(typeid(additive_rollup_comp<multiply_comp<small_company, 2>::type>::type) ==
-// 	typeid(small_company));
-// 	assert(typeid(additive_expand_comp<small_company>::type) ==
-// 	typeid(add_comp<Hunting_shop, multiply_comp<small_company, 2>::type>::type));
+ 	assert(typeid(add_comp<Accountancy, Exchange_office>::type) == typeid(small_company));
+ 	assert(typeid(remove_comp<small_company, Accountancy>::type) == typeid(Exchange_office));
+ 	assert(typeid(split_comp<small_company, 2>::type) == typeid(Company<0, 0, 0>));
+ 	assert(typeid(additive_rollup_comp<multiply_comp<small_company, 2>::type>::type) ==
+ 	typeid(small_company));
+ 	assert(typeid(additive_expand_comp<small_company>::type) ==
+ 	typeid(add_comp<Hunting_shop, multiply_comp<small_company, 2>::type>::type));
 //
-// 	assert(typeid(Group<add_comp<Accountancy, Exchange_office>::type>::company_type) ==
-// 	typeid(small_company));
-// 	assert(typeid(additive_expand_group(s1).company) ==
-// 	typeid(add_comp<multiply_comp<Accountancy, 2>::type,
-// 	       add_comp<Hunting_shop, Exchange_office>::type>::type));
-// 	assert(typeid(additive_rollup_group(s1).company) == typeid(Company<0,0,0>));
-// 	assert(typeid(multiplicative_expand_group(s1).company) ==
-// 	typeid(multiply_comp<Accountancy, 10>::type));
-// 	assert(typeid(multiplicative_rollup_group(s5).company) ==
-// 	typeid(add_comp<multiply_comp<Hunting_shop, 3>::type, Exchange_office>::type));
+ 	assert(typeid(Group<add_comp<Accountancy, Exchange_office>::type>::company_type) ==
+ 	typeid(small_company));
+ 	/*assert(typeid(additive_expand_group(s1).company) ==
+ 	typeid(add_comp<multiply_comp<Accountancy, 2>::type,
+ 	       add_comp<Hunting_shop, Exchange_office>::type>::type));
+ 	assert(typeid(additive_rollup_group(s1).company) == typeid(Company<0,0,0>));
+ 	assert(typeid(multiplicative_expand_group(s1).company) ==
+		typeid(multiply_comp<Accountancy, 10>::type));
+ 	assert(typeid(multiplicative_rollup_group(s5).company) ==
+		typeid(add_comp<multiply_comp<Hunting_shop, 3>::type, Exchange_office>::type));*/
 //
 // 	assert(solve_auction(s1, s2, s5) == false);
 
