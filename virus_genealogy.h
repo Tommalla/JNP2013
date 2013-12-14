1ffb14a1a14b2b154aaee289883a1625f28870b0
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2013-12-14 21:59:01 +0100

    Some reformatting and minor changes.

--- virus_genealogy.h
+++ virus_genealogy.h
@@ -27,47 +27,50 @@ class VirusGenealogy {
 	VirusGenealogy& operator=(VirusGenealogy<Virus> const &other) = delete;
 
 	class Node {
-
 	public:
-		const Virus vir;
+		typedef weak_ptr<Node> WeakPtr;
+		typedef shared_ptr<Node> SharedPtr;
+		typedef set<WeakPtr, std::owner_less<WeakPtr>> ParentSet;
+		typedef set<SharedPtr> ChildrenSet;	//FIXME this can be an unordered_set (see the doc, esp. the Hash function)
 
-		Node(typename Virus::id_type const &id) : vir(id), id{id} {} //for stem
+		Virus vir;
 
-		Node(typename Virus::id_type const &id, weak_ptr<Node> parent) : vir(id), id{id} {
+		Node(typename Virus::id_type const &id) : vir{id}, id{id} {} //for stem
+
+		Node(typename Virus::id_type const &id, WeakPtr const &parent) : vir{id}, id{id} {
 			parents.insert(parent);
 		}
 
-		Node(typename Virus::id_type const &id, set<weak_ptr<Node>, std::owner_less<weak_ptr<Node>>> parent_set ) : vir(id), id{id} {
-			
-			parents = std::move(parent_set);
-		}
-
-		//TODO second ctor
+		Node(typename Virus::id_type const &id, ParentSet const &parent_set) : vir{id},
+			id{id}, parents(parent_set) {}
 
 		const typename Virus::id_type id;
-		set<shared_ptr<Node>> children;	//FIXME this can be an unordered_set (see the doc, esp. the Hash function)
-		set<weak_ptr<Node>, std::owner_less<weak_ptr<Node>>> parents;
+		ChildrenSet children;
+		ParentSet parents;
 	};
 
 	const typename Virus::id_type stem_id;
-	typedef map<typename Virus::id_type, weak_ptr<Node>> Graph;
+	typedef map<typename Virus::id_type, typename Node::WeakPtr> Graph;
 	Graph nodes;
-	shared_ptr<Node> stem;
+	typename Node::SharedPtr stem;
 
  	typename Graph::const_iterator get_iterator(typename Virus::id_type const &id) const {
 		auto iter = nodes.find(id);
+
 		if(iter != nodes.end())
 			if(iter->second.use_count() == 0)
 				iter = nodes.end();
-		if(iter == nodes.end()) 
+
+		if(iter == nodes.end())
 			throw VirusNotFound();
+
 		return iter;
 	}
 
 public:
 	//constructor
-	VirusGenealogy(typename Virus::id_type const &stem_id) : stem_id{stem_id}, stem( new Node(stem_id) ) {
-		nodes.emplace(stem_id, weak_ptr<Node>(stem));	//FIXME add strong guarantee
+	VirusGenealogy(typename Virus::id_type const &stem_id) : stem_id{stem_id}, stem{new Node(stem_id)} {
+		nodes.emplace(stem_id, typename Node::WeakPtr(stem));	//FIXME add strong guarantee
 	}
 
 	//getters
@@ -94,42 +97,41 @@ public:
 	}
 
 	void create(typename Virus::id_type const &id, std::vector<typename Virus::id_type> const &parent_ids) {
-		if(parent_ids.size() == 0) throw VirusNotFound();
-		typename Graph::const_iterator iter;
-		set<weak_ptr<Node>, std::owner_less<weak_ptr<Node>>> parent_set;
-		for(auto ptr = parent_ids.begin(); ptr != parent_ids.end(); ptr++)
-			parent_set.insert( weak_ptr<Node> ( get_iterator( *ptr )->second ) );
+		if(parent_ids.size() == 0)
+			throw VirusNotFound();
 
-		shared_ptr<Node> sp( new Node(id, parent_set) );
+		typename Graph::const_iterator iter;
+		typename Node::ParentSet parent_set;
+		for(auto ptr = parent_ids.begin(); ptr != parent_ids.end(); ptr++)
+			parent_set.insert(typename Node::WeakPtr( get_iterator( *ptr )->second ) );
+
+		typename Node::SharedPtr sp( new Node(id, parent_set) );
 		for(auto ptr = parent_set.begin(); ptr != parent_set.end(); ptr++)
-			ptr->lock()->children.insert(shared_ptr<Node>(sp));
-		
-		nodes.emplace(id, weak_ptr<Node>(sp));
+			ptr->lock()->children.insert(typename Node::SharedPtr(sp));
+
+		nodes.emplace(id, typename Node::WeakPtr(sp));
 	}
 
 	//TODO try & catch on get_iterator here
 	bool exists(typename Virus::id_type const &id) const {
-		return get_iterator(id)!=nodes.end();
+		return get_iterator(id) != nodes.end();
 	}
-	
+
 	void create(typename Virus::id_type const &id, typename Virus::id_type const &parent_id) {
 		auto iter = get_iterator(parent_id);
-		shared_ptr<Node> sp( new Node(id, iter->second ));
-		nodes.emplace(id, weak_ptr<Node>(sp));
+		typename Node::SharedPtr sp( new Node(id, iter->second ));
+		nodes.emplace(id, typename Node::WeakPtr(sp));
 		iter->second.lock()->children.insert(sp);
 	}
 
-
-
 	void connect(typename Virus::id_type const &child_id, typename Virus::id_type const &parent_id) {
 		auto parent = get_iterator(parent_id), child = get_iterator(child_id);
-		parent->second.lock()->children.insert( shared_ptr<Node> ( child->second ) );
-		child->second.lock()->parents.insert( weak_ptr<Node> ( parent->second ) );
+		parent->second.lock()->children.insert( typename Node::SharedPtr( child->second ) );
+		child->second.lock()->parents.insert( typename Node::WeakPtr( parent->second ) );
 	}
 
-
 	//TODO sluzy do debugu, usunac na koncu
-	int get_usage(typename Virus::id_type const &id){
+	int get_usage(typename Virus::id_type const &id) {
 		auto iter = get_iterator(id);
 		return iter->second.use_count();
 	}
