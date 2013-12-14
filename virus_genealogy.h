2e6d5c5a8a82c4beaac05f349b4fe39f27a3c939
Michal Stefanczyk <michal.stefanczyk93@gmail.com>
2013-12-14 14:53:37 +0100

    first change

--- virus_genealogy.h
+++ virus_genealogy.h
@@ -22,34 +22,9 @@ class TriedToRemoveStemVirus : public exception {};
 
 template<class Virus>
 class VirusGenealogy {
-public:
-	//constructor
-	VirusGenealogy(typename Virus::id_type const &stem_id) : stem_id{stem_id} {
-		nodes.emplace(stem_id, Node(stem_id, nullptr));	//FIXME add strong guarantee
-	}
 
-	//getters
-	typename Virus::id_type get_stem_id() const noexcept {
-		return stem_id;
-	}
-
-	std::vector<typename Virus::id_type> get_children(typename Virus::id_type const &id) const {
-		auto iter = nodes.find(id);
-		if (iter == nodes.end())
-			throw VirusNotFound();
-
-		vector<typename Virus::id_type> res;
-		for (auto const &ptr: iter->children)
-			res.push_back(ptr->id);
-
-		return res;
-	}
-
-	//TODO the rest of the methods
-
-private:
-	VirusGenealogy(VirusGenealogy<Virus> const &other);
-	VirusGenealogy& operator=(VirusGenealogy<Virus> const &other);
+	VirusGenealogy(VirusGenealogy<Virus> const &other) = delete;
+	VirusGenealogy& operator=(VirusGenealogy<Virus> const &other) = delete;
 
 	const typename Virus::id_type stem_id;
 
@@ -69,6 +44,46 @@ private:
 
 	typedef map<typename Virus::id_type, Node> Graph;
 	Graph nodes;
+
+public:
+	//constructor
+	VirusGenealogy(typename Virus::id_type const &stem_id) : stem_id{stem_id} {
+		nodes.emplace(stem_id, Node(stem_id, nullptr));	//FIXME add strong guarantee
+	}
+
+	//getters
+	typename Virus::id_type get_stem_id() const noexcept {
+		return stem_id;
+	}
+
+	std::vector<typename Virus::id_type> get_children(typename Virus::id_type const &id) const {
+		auto iter = nodes.find(id);
+		if (iter == nodes.end())
+			throw VirusNotFound();
+
+		vector<typename Virus::id_type> res;
+		for (auto const &ptr: iter->second.children)
+			res.push_back(ptr->id);
+
+		return res;
+	}
+
+	std::vector<typename Virus::id_type> get_parents(typename Virus::id_type const &id) const {
+		auto iter = nodes.find(id);
+		if (iter == nodes.end())
+			throw VirusNotFound();
+
+		vector<typename Virus::id_type> res;
+		for (auto const &ptr: iter->second.parents)
+			res.push_back(ptr.lock()->id);
+
+		return res;
+	}
+
+	//TODO the rest of the methods
+	bool exists(typename Virus::id_type const &id) const {
+		return nodes.find(id) != nodes.end();
+	}
 };
 
 #endif
