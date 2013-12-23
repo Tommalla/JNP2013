6aaf8699d065bb13c60fcd66cdf7a7dc3c1e641e
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2013-12-23 23:31:57 +0100

    Improved tests.

--- private/main.cpp
+++ private/main.cpp
@@ -16,7 +16,59 @@ private:
 	id_type id;
 };
 
+void extra() {
+	VirusGenealogy<Virus> gen("A1H1");
+	Virus::id_type const id1 = gen.get_stem_id();
+
+	assert(gen.exists(id1));
+	assert(gen.get_parents(id1).size() == 0);
+	gen.create("A", id1);
+	gen.create("B", id1);
+	assert(gen.get_children(id1).size() == 2);
+	gen.create("C", "A");
+	gen.connect("C", "B");
+	assert(gen.get_parents("C").size() == 2);
+	assert(gen.get_children("A").size() == 1);
+	std::vector<typename Virus::id_type> parents;
+	parents.push_back("A");
+	parents.push_back("B");
+	gen.create("D", parents);
+	assert(gen.get_parents("D").size() == parents.size());
+	assert(gen.get_children("A").size() == 2);
+	assert(gen["D"].get_id() == "D");
+ 	gen.remove("A");
+	assert(!gen.exists("A"));
+	assert(gen.exists("B"));
+	assert(gen.exists("C"));
+	assert(gen.exists("D"));
+	gen.remove("B");
+	assert(!gen.exists("A"));
+	assert(!gen.exists("B"));
+	assert(!gen.exists("C"));
+	assert(!gen.exists("D"));
+	try {
+		gen["E"];
+	}
+	catch (VirusNotFound const &e) {
+		assert(e.what() == std::string("VirusNotFound"));
+	}
+	try {
+		gen.create("E", "A1H1");
+		gen.create("E", "A1H1");
+	}
+	catch (VirusAlreadyCreated const &e) {
+		assert(e.what() == std::string("VirusAlreadyCreated"));
+	}
+	try {
+		gen.remove("A1H1") ;
+	}
+	catch (TriedToRemoveStemVirus const &e) {
+		assert(e.what() == std::string("TriedToRemoveStemVirus"));
+	}
+}
+
 int main() {
+	extra();
 	VirusGenealogy<Virus> gen("A1H1");
 	Virus::id_type const id1 = gen.get_stem_id();
 	assert(gen.exists(id1));
