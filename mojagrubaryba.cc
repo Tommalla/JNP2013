cc5ab275a7d2b00af81ef291d8de208c3b6e6225
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2014-01-24 00:04:32 +0100

    Initial commit.

---
+++ mojagrubaryba.cc
@@ -0,0 +1,529 @@
+/* Tomasz Zakrzewski (tz336079) i Andrzej Białokozowicz (ab333903)
+ * JNP1 2013/2014, Zadanie 6
+ */
+#include "mojagrubaryba.h"
+#include <sstream>
+#include <iostream>
+
+using std::ostringstream;
+
+//MojaGrubaRyba -------------------------------------------------------------------------
+
+MojaGrubaRyba::MojaGrubaRyba() {
+	board = unique_ptr<Board>(new Board(this));
+}
+
+
+void MojaGrubaRyba::setDie(std::shared_ptr< Die > die) {
+	this->dieCopy = die;
+}
+
+void MojaGrubaRyba::addComputerPlayer(GrubaRyba::ComputerLevel level) {
+	//FIXME
+	//Dodac wyjatek toomanyplayers
+	shared_ptr<Player> player;
+	ostringstream convert;
+	string name;
+	convert << "Gracz "<< players.size();
+	name = convert.str();
+
+	switch(level) {
+		case GrubaRyba::ComputerLevel::DUMB:
+			player = shared_ptr<Player> (new DumbComputerPlayer(1000, board->getInitialPosition(), name));
+			break;
+		case GrubaRyba::ComputerLevel::SMARTASS:
+			player = shared_ptr<Player> (new SmartComputerPlayer(1000, board->getInitialPosition(), name));
+			break;
+	}
+	players.push_back(player);
+}
+
+void MojaGrubaRyba::addHumanPlayer(std::shared_ptr< Human > human) {
+	//FIXME
+	//Dodac wyjatek toomanyplayers
+	shared_ptr<Player> player = shared_ptr<Player>(new HumanPlayer(1000, board->getInitialPosition(), human));
+	players.push_back(player);
+}
+
+void MojaGrubaRyba::play(unsigned int rounds) {
+	reset();
+
+	while (rounds--)
+		for (PlayerId id = getFirstPlayer(); id != getWatchdogPlayer(); id = getNextPlayerId(id)) {
+			if (players.at(id)) {	//jeśli jeszcze gra
+				unsigned int steps = rollDies();
+				auto moveRes = board->makeMove(id, players.at(id)->getPosition(), steps);
+				shared_ptr<Player> p;
+
+				if (p = players.at(id)) {	//jeśli gracz po turze dalej jest w grze
+					p->setPosition(moveRes.first);
+					unsigned int rounds;
+					if ((rounds = moveRes.second->roundsToWait) > 0)
+						p->wait(rounds);
+					else if (board->canBeBought(moveRes.first)) {
+						auto name = board->getFieldName(moveRes.first);
+						auto cost = board->getBuyValue(moveRes.first);
+						if (p->wantBuy(name) && p->getMoney() >= cost) {
+							p->setMoney(p->getMoney() - cost);
+							p->addProperty(moveRes.first);
+							board->own(id, moveRes.first);
+						}
+					}
+				}
+			}
+		}
+}
+
+Money MojaGrubaRyba::takeMoneyFrom(const PlayerId& id, const Money& sum) {
+	auto p = getPlayerAt(id);
+
+	if (p->getMoney() >= sum) {
+		p->setMoney(p->getMoney() - sum);
+		return sum;
+	}
+
+	//player nie ma dosyć środków, musi zbankrutować
+	Money debt = makeBankrupt(p);
+	if (debt >= sum)
+		return -1;
+	return debt;
+}
+
+Money MojaGrubaRyba::transferMoney(const PlayerId& from, const PlayerId& to, const Money& sum) {
+	Money debt = takeMoneyFrom(from, sum);
+
+	if (debt == -1)
+		debt = sum;
+
+	giveMoneyTo(to, debt);
+
+	return debt;
+}
+
+void MojaGrubaRyba::giveMoneyTo(const PlayerId& id, const Money& sum) {
+	auto p = getPlayerAt(id);
+	p->setMoney(p->getMoney() + sum);
+}
+
+unsigned int MojaGrubaRyba::rollDies() {
+	return die->roll() + die->roll();
+}
+
+Money MojaGrubaRyba::makeBankrupt(shared_ptr< Player >& p){
+	Money res = sellPropertiesOf(p);
+	p = nullptr;	//usuwamy playera
+	return res;
+}
+
+Money MojaGrubaRyba::sellPropertiesOf(shared_ptr<Player>& p) {
+	Money res = 0;
+	auto properties = p->getProperties();
+	for(const auto &pos: properties) {
+		auto name = board->getFieldName(pos);
+		if (p->wantSell(name)) {	//próbujemy sprzedawać wszystko
+			res += board->getSellValue(pos);
+			board->deown(pos);
+		}
+	}
+
+	return res;
+}
+
+shared_ptr< Player > MojaGrubaRyba::getPlayerAt(const PlayerId& id) const {
+	auto p = players.at(id);
+	if (!p)
+		throw IllegalOperationException();
+	return p;
+}
+
+PlayerId MojaGrubaRyba::getFirstPlayer() {
+	return 0;
+}
+
+PlayerId MojaGrubaRyba::getWatchdogPlayer() {
+	return players.size();
+}
+
+PlayerId MojaGrubaRyba::getNextPlayerId(const PlayerId& id) {
+	return id + 1;
+}
+
+void MojaGrubaRyba::reset() {
+	players = vector<shared_ptr<Player>>(playersCopy);
+	board = unique_ptr<Board>(new Board(this));
+	die = dieCopy->clone();
+}
+
+// Koniec (MojaGrubaRyba) ---------------------------------------------------------------
+// Board --------------------------------------------------------------------------------
+
+Board::Board(MojaGrubaRyba* gameMaster) : gameMaster{gameMaster} {
+	//TODO
+	fields.push_back(shared_ptr<Field>(new StartField("Start", 50)));
+// 	fields.push_back(shared_ptr<Field>)
+}
+
+MoveResult Board::makeMove(const PlayerId& id, const BoardPosition& pos, unsigned int steps) {
+	MoveResult res;
+	res.first = pos;
+
+	while (steps--) {
+		res.first = getNext(res.first);
+
+		res.second = (steps == 0) ? getFieldAt(res.first)->stop() : getFieldAt(res.first)->pass();
+
+		if (res.second->accountChange != 0) {
+			Money sum = res.second->accountChange;
+			if (sum > 0)	//dostajemy pieniądze
+				gameMaster->giveMoneyTo(id, sum);
+			else {
+				Money paid;
+
+				auto f = getFieldAt(res.first);
+				if (f->isOwned())	//płacimy komuś trybut
+					paid = gameMaster->transferMoney(id, f->getOwner(), sum);
+				else	//płacimy rządowi i pieniądze gdzieś magicznie znikają!!
+					f->store(paid = gameMaster->takeMoneyFrom(id, sum));
+
+				if (paid < sum)
+					return res;	//zbankrutował gracz, przerywamy
+			}
+
+			res.second->accountChange = 0;	//cokolwiek było do zapłacenia, zapłaciliśmy
+		}
+	}
+
+	return res;
+}
+
+bool Board::canBeBought(const BoardPosition& pos) {
+	return !getFieldAt(pos)->isOwned();
+}
+
+string Board::getFieldName(const BoardPosition& pos) {
+	return getFieldAt(pos)->getName();
+}
+
+Money Board::getSellValue(const BoardPosition& pos) {
+	return getFieldAt(pos)->getCost() / 2;
+}
+
+Money Board::getBuyValue(const BoardPosition& pos) {
+	return getFieldAt(pos)->getCost();
+}
+
+void Board::own(const PlayerId& id, const BoardPosition& pos) {
+	getFieldAt(pos)->setOwner(id);
+}
+
+void Board::deown(const BoardPosition& pos) {
+	getFieldAt(pos)->removeOwner();
+}
+
+BoardPosition Board::getInitialPosition() const {
+	return 0;
+}
+
+Board::~Board() {
+	gameMaster = nullptr;	//we don't own our parent
+}
+
+shared_ptr< Field > Board::getFieldAt(const BoardPosition& pos) const {
+	return fields.at(pos);
+}
+
+BoardPosition Board::getNext(const BoardPosition& pos) const {
+	return (pos + 1) % fields.size();
+}
+
+BoardPosition Board::getBoardSize() const {
+	return fields.size();
+}
+
+
+// Koniec (Board) -----------------------------------------------------------------------
+// Player -------------------------------------------------------------------------------
+
+Player::Player(const Money& money, const BoardPosition& position)
+	: money{money}
+	, position{position} {}
+
+void Player::wait(unsigned int roundsToWait) {
+	this->roundsToWait += roundsToWait;
+}
+
+bool Player::isWaiting() {
+	if (roundsToWait > 0)
+		roundsToWait--;
+	return (roundsToWait == 0);
+}
+
+void Player::setMoney(const Money& sum) {
+	if (sum < 0)
+		throw IllegalOperationException();
+	money = sum;
+}
+
+Money Player::getMoney() const {
+	return money;
+}
+
+BoardPosition Player::getPosition() {
+	return position;
+}
+
+void Player::setPosition(const BoardPosition& pos) {
+	position = pos;
+}
+
+vector< BoardPosition > Player::getProperties() const {
+	return properties;
+}
+
+void Player::addProperty(const BoardPosition& pos) {
+	properties.push_back(pos);
+}
+
+
+// Koniec (Player) ----------------------------------------------------------------------
+// HumanPlayer
+HumanPlayer::HumanPlayer(const Money& money, const BoardPosition &pos, shared_ptr< Human >& human)
+	: Player{money, pos} {
+		this->human = human->clone();
+	}
+
+bool HumanPlayer::wantBuy(const string& propertyName) {
+	return human->wantBuy(propertyName);
+}
+
+bool HumanPlayer::wantSell(const string& propertyName) {
+	return human->wantSell(propertyName);
+}
+
+string const& HumanPlayer::getName() const {
+	return human->getName();
+}
+
+// Koniec (HumanPlayer)
+// ComputerPlayer
+ComputerPlayer::ComputerPlayer(const Money& money, const BoardPosition &pos, const string& name)
+	: Player(money, pos)
+	, name{name} {}
+
+string const& ComputerPlayer::getName() const {
+	return name;
+}
+
+// Koniec ComputerPlayer
+// DumbComputerPlayer
+DumbComputerPlayer::DumbComputerPlayer(const Money& money, const BoardPosition &pos, const string& name)
+	: ComputerPlayer{money, pos, name}
+	, counter{0} {}
+
+bool DumbComputerPlayer::wantBuy(const string& propertyName) {
+	counter = (counter + 1) % 3;
+	return (counter == 0);
+}
+
+bool DumbComputerPlayer::wantSell(const string& propertyName) {
+	return false;
+}
+
+// Koniec DumbComputerPlayer
+// SmartComputerPlayer
+SmartComputerPlayer::SmartComputerPlayer(const Money& money, const BoardPosition &pos, const string& name)
+	: ComputerPlayer{money, pos, name} {}
+
+bool SmartComputerPlayer::wantBuy(const string& propertyName) {
+	return true;
+}
+
+bool SmartComputerPlayer::wantSell(const string& propertyName) {
+	return false;
+}
+
+// Koniec (SmartComputerPlayer)
+// Field --------------------------------------------------------------------------------
+
+Field::Field(const string& name, const Money cost)
+	: name{name}
+	, cost{cost} {}
+
+Field::~Field() {
+}
+
+effectPtr Field::pass()
+{
+	effectPtr ptr(new EffectInfo(0, 0));
+	return ptr;
+}
+
+const Money Field::getCost() const {
+	return cost;
+}
+
+const string& Field::getName() const {
+	return name;
+}
+
+const PlayerId Field::getOwner() const {
+	return ownerId;
+}
+
+bool Field::isOwned() {
+	return owned;
+}
+
+void Field::removeOwner() {
+	owned = false;
+}
+
+void Field::setOwner(const PlayerId& ownerId) {
+	owned = true;
+	this->ownerId = ownerId;
+}
+
+void Field::store(Money amount) {
+}
+
+// Koniec (Field) -----------------------------------------------------------------------
+// NotOwnedField ------------------------------------------------------------------------
+
+NotOwnedField::NotOwnedField(const string& name)
+	: Field(name, 0) {}
+
+NotOwnedField::~NotOwnedField() {
+}
+
+bool NotOwnedField::isOwned() {
+	return false;
+}
+
+// Koniec (NotOwnedField) ---------------------------------------------------------------
+// StartField ---------------------------------------------------------------------------
+
+StartField::StartField(const string& name, Money prize)
+	: NotOwnedField(name)
+	, prize{prize} {}
+
+effectPtr StartField::pass()
+{
+	effectPtr ptr(new EffectInfo(prize, 0));
+	return ptr;
+}
+
+effectPtr StartField::stop() {
+	return this->pass();
+}
+
+// Koniec (StartField) ------------------------------------------------------------------
+// AwardField ---------------------------------------------------------------------------
+
+AwardField::AwardField(const string& name, Money prize)
+	: NotOwnedField(name)
+	, prize{prize} {}
+
+effectPtr AwardField::stop()
+{
+	effectPtr ptr(new EffectInfo(prize, 0));
+	return ptr;
+}
+
+// Koniec (AwardField) ------------------------------------------------------------------
+// PenaltyField -------------------------------------------------------------------------
+
+PenaltyField::PenaltyField(const string& name, Money penalty)
+	: NotOwnedField(name)
+	, penalty{penalty} {}
+
+effectPtr PenaltyField::stop() {
+	effectPtr ptr(new EffectInfo(-penalty, 0));
+	return ptr;
+}
+
+// Koniec (PenaltyField) ----------------------------------------------------------------
+// DepositField -------------------------------------------------------------------------
+
+DepositField::DepositField(const string& name, Money penalty)
+	: NotOwnedField(name)
+	, penalty{penalty} {}
+
+effectPtr DepositField::pass()
+{
+	effectPtr ptr(new EffectInfo(-penalty, 0));
+	return ptr;
+}
+
+effectPtr DepositField::stop()
+{
+	effectPtr ptr(new EffectInfo(currentSum, 0));
+	currentSum = 0;
+	return ptr;
+}
+
+void DepositField::store(Money amount) {
+	currentSum += amount;
+}
+
+// Koniec (DepositField) ----------------------------------------------------------------
+// AquariumField ------------------------------------------------------------------------
+
+AquariumField::AquariumField(const string& name, unsigned int rounds)
+	: NotOwnedField(name)
+	, rounds{rounds} {}
+
+effectPtr AquariumField::stop() {
+	effectPtr ptr(new EffectInfo(0, rounds));
+	return ptr;
+}
+
+// Koniec (AquariumField) ---------------------------------------------------------------
+// PropertyField ------------------------------------------------------------------------
+
+PropertyField::PropertyField(const string& name, const Money cost, const Money penalty)
+	: Field(name, cost)
+	, penalty{penalty} {}
+
+// Koniec (PropertyField) ---------------------------------------------------------------
+// PublicPropertyField ------------------------------------------------------------------
+PublicPropertyField::PublicPropertyField(const string& name, const Money cost)
+	: PropertyField(name, cost, (cost * 40) / 100) {}
+
+effectPtr PublicPropertyField::stop() {
+	Money amount;
+	if (this->isOwned())
+		amount = -penalty;
+	else
+		amount = -cost;
+	effectPtr ptr(new EffectInfo(amount, 0));
+	return ptr;
+}
+
+// Koniec (PublicPropertyField) ---------------------------------------------------------
+// CoralField ---------------------------------------------------------------------------
+CoralField::CoralField(const string& name, const Money cost)
+	: PropertyField(name, cost, (cost * 20) / 100) {}
+
+effectPtr CoralField::stop() {
+	Money amount;
+	if (this->isOwned())
+		amount = -penalty;
+	else
+		amount = -cost;
+	effectPtr ptr(new EffectInfo(amount, 0));
+	return ptr;
+}
+
+effectPtr IslandField::stop() {
+	return effectPtr(new EffectInfo(0, 0));
+}
+
+
+// Koniec (CoralField) ------------------------------------------------------------------
+// EffectInfo ---------------------------------------------------------------------------
+
+EffectInfo::EffectInfo(const Money& accountChange, const unsigned int roundsToWait)
+	: roundsToWait{roundsToWait}
+	, accountChange{accountChange} {}
+
+//Koniec (EffectInfo) -------------------------------------------------------------------
---
+++ mojagrubaryba.h
@@ -0,0 +1,368 @@
+/* Tomasz Zakrzewski (tz336079) i Andrzej Białokozowicz (ab333903)
+ * JNP1 2013/2014, Zadanie 6
+ */
+#ifndef MOJAGRUBARYBA_H
+#define MOJAGRUBARYBA_H
+
+#include <cstdio>
+#include <vector>
+#include <list>
+#include <string>
+#include <exception>
+
+#include "grubaryba.h"
+
+using std::vector;
+using std::list;
+using std::shared_ptr;
+using std::max;
+using std::unique_ptr;
+using std::pair;
+using std::string;
+
+class Board;
+class Player;
+class Field;
+struct EffectInfo;
+typedef size_t PlayerId;
+typedef size_t BoardPosition;
+typedef int Money;
+typedef pair<BoardPosition, shared_ptr<EffectInfo>> MoveResult;
+typedef shared_ptr <EffectInfo> effectPtr;
+
+//wyjątek rzucany, jeśli czyjaś implementacja, używając naszych klas,
+//
+class IllegalOperationException : public std::exception {
+public:
+	IllegalOperationException() {}
+	const char* what() const noexcept {
+		return "Violated one of the contracts";
+	}
+};
+
+struct EffectInfo {
+	unsigned int roundsToWait;
+
+	//> 0 -> gracz dostaje pieniądze
+	//< 0 -> gracz płaci
+	Money accountChange;
+
+	//pomocniczy konstruktor
+	EffectInfo(const Money& accountChange, const unsigned int roundsToWait);
+	EffectInfo(){};
+};
+
+class MojaGrubaRyba : public GrubaRyba {
+public:
+	MojaGrubaRyba();
+
+	virtual void setDie(shared_ptr<Die> die);
+	virtual void addComputerPlayer(ComputerLevel level);
+	virtual void addHumanPlayer(shared_ptr<Human> human);
+	virtual void play(unsigned int rounds);
+
+	//Dwie poniższe metody służą do transferu pieniędzy między graczami/zabierania pieniędzy graczowi.
+	//Metody zwracają sumę pieniędzy jaka została przekazana/odebrana (może być < sum) lub -1, w przypadku
+	//gdy przekazana suma była równa sum, ale gracz musiał zbankrutować, aby ją wypłacić.
+	//Obie metody rzucają wyjątkami, jeśli argumentem jest nieistniejący (nigdy nie stworzony, lub taki który
+	//już przegrał) gracz
+	Money takeMoneyFrom(const PlayerId& id, const Money& sum);
+	Money transferMoney(const PlayerId& from, const PlayerId& to, const Money& sum);
+
+	void giveMoneyTo(const PlayerId& id, const Money& sum);
+
+	//Metoda rzuca kostkami i zwraca wynik rzutu.
+	unsigned int rollDies();
+
+private:
+	//Gracz p bankrutuje.
+	//Metoda zwraca ilość pieniędzy odzyskanych od niego ze sprzedawania nieruchomości
+	Money makeBankrupt(shared_ptr<Player>& p);
+
+	//Pyta gracza o każdą z jego nieruchomości i próbuje je sprzedać.
+	//Zwraca ilość uzbieranych pieniędzy.
+	Money sellPropertiesOf(shared_ptr< Player >& p);
+
+	//Zwraca shared_ptr na gracza o danym id
+	shared_ptr<Player> getPlayerAt(const PlayerId& id) const;
+
+	//nie-const, bo nie wiadomo jak ktoś kiedyś chce trzymać playerów
+	//może np. chcieć coś sobie przetwarzać przed zwróceniem pierwszego
+	PlayerId getFirstPlayer();
+
+	//zwraca id watchdoga - końca zbioru playerów
+	PlayerId getWatchdogPlayer();
+
+	PlayerId getNextPlayerId(const PlayerId& id);
+
+	//przywróć planszę do stanu początkowego
+	void reset();
+
+	shared_ptr<Die> die;
+	shared_ptr<Die> dieCopy;
+	vector<shared_ptr<Player>> players;
+	//przechowuje kopię playerów z oryginalnymi wartościami
+	//na potrzeby gry od nowa
+	vector<shared_ptr<Player>> playersCopy;
+	unique_ptr<Board> board;
+};
+
+
+class Board {
+public:
+	Board(MojaGrubaRyba* gameMaster);
+
+	//Wykonuje ruch z perspektywy planszy - nie ma dostępu do playera, wie tylko że jakiś obiekt
+	//identyfikowalny przez od chce wykonać ruch ze swojej obecnej pozycji pos o steps pól do przodu.
+	//Możliwość wykonania każdego ruchu będzie walidowana w MojejGrubejRybie.
+	//FIXME tu jest zaszyta logika tego co się dzieje w grze,  więc i możliwości.
+	//W związku z tym ta klasa powinna mieć część metod (włącznie z tą) wirtualnych oraz
+	//wirtualny destruktor, bo ktoś chcący dodać nowe rodzaje pól z nieznanymi wcześniej efektami, będzie musiał przedefiniować
+	//tę metodę, żeby te pola działały
+	MoveResult makeMove(const PlayerId &id, const BoardPosition& pos, unsigned int steps);
+
+
+	virtual bool canBeBought(const BoardPosition &pos);
+
+	string getFieldName(const BoardPosition &pos);
+	Money getSellValue(const BoardPosition &pos);
+	Money getBuyValue(const BoardPosition &pos);
+
+	//Przypisuje pole na pozycji pos graczowi id
+	void own(const PlayerId &id, const BoardPosition &pos);
+	//Zwalnia pole na pozycji pos (pole traci poprzedniego)
+	//właściciela
+	void deown(const BoardPosition &pos);
+
+	BoardPosition getInitialPosition() const;
+
+	~Board();
+private:
+	//Metoda zwracająca kolejne pole (abstrakcja BoardPosition)
+	BoardPosition getNext(const BoardPosition& pos) const;
+
+	//Metoda zwracająca rozmiar planszy (abstrakcja wokół std::vector)
+	BoardPosition getBoardSize() const;
+
+	shared_ptr<Field> getFieldAt(const BoardPosition& pos) const;
+
+	MojaGrubaRyba* gameMaster;
+
+	vector<shared_ptr <Field> >fields;
+};
+
+
+class Player {
+public:
+	Player(const Money& money, const BoardPosition& position);
+	virtual ~Player(){}
+
+	// Zwraca imię gracza
+	virtual std::string const& getName() const = 0;
+
+	// Zwraca true, jeśli gracz chce kupić daną posiadłość.
+	virtual bool wantBuy(std::string const& propertyName) = 0;
+
+	// Zwraca true, jeśli gracz chce sprzedać daną posiadłość.
+	// Wywoływane w przypadku, gdy brakuje graczowi pieniędzy na zakup lub opłaty.
+	virtual bool wantSell(std::string const& propertyName) = 0;
+
+	void wait(unsigned int roundsToWait);
+	bool isWaiting();
+
+	//Ustawia stan konta gracza na sum. Nielegalnym jest ustawianie konta na ujemną
+	//wartość. W takim przypadku, metoda rzuca IllegalOperationException i stan konta
+	//gracza pozostaje niezmieniony.
+	void setMoney(const Money &sum);
+	Money getMoney() const;
+
+	BoardPosition getPosition();
+	void setPosition(const BoardPosition& pos);
+
+	//przez kopię, bo może się zmieniać
+	vector<BoardPosition> getProperties() const;
+	void addProperty(const BoardPosition& pos);
+private:
+	unsigned int roundsToWait;
+	Money money;
+	BoardPosition position;
+	vector<BoardPosition> properties;
+};
+
+class HumanPlayer : public Player {
+
+public:
+	HumanPlayer(const Money& money, const BoardPosition& pos, shared_ptr< Human >& human);
+
+	virtual bool wantBuy(string const& propertyName);
+	virtual bool wantSell(string const& propertyName);
+	string const& getName() const;
+
+private:
+	shared_ptr <Human> human;
+};
+
+class ComputerPlayer : public Player {
+
+public:
+	ComputerPlayer(const Money& money, const BoardPosition& pos, const string& name);
+
+	virtual bool wantBuy(string const& propertyName) = 0;
+	virtual bool wantSell(string const& propertyName) = 0;
+	string const& getName() const;
+
+private:
+	string name;
+};
+
+class DumbComputerPlayer : public ComputerPlayer
+{
+public:
+	DumbComputerPlayer(const Money& money, const BoardPosition& pos, const string& name);
+
+	virtual bool wantBuy(string const& propertyName);
+	virtual bool wantSell(string const& propertyName);
+
+private:
+	int counter;
+};
+
+
+class SmartComputerPlayer : public ComputerPlayer
+{
+public:
+
+	SmartComputerPlayer(const Money& money, const BoardPosition& pos, const string& name);
+
+	virtual bool wantBuy(string const& propertyName);
+	virtual bool wantSell(string const& propertyName);
+};
+
+
+class Field {
+public:
+	Field(const string& name, const Money cost);
+	virtual ~Field();
+
+	virtual effectPtr pass();
+	virtual effectPtr stop() = 0;
+	virtual bool isOwned();
+	virtual void store(Money amount);
+
+	const PlayerId getOwner() const;
+	const Money getCost() const;
+	const string& getName() const;
+
+	void setOwner(const PlayerId& ownerId);
+	void removeOwner();
+
+private:
+	PlayerId ownerId;
+	string name;
+	bool owned;
+
+protected:
+	Money cost;
+};
+
+
+class NotOwnedField : public Field {
+public:
+	NotOwnedField(const string& name);
+	virtual ~NotOwnedField();
+
+	virtual effectPtr stop() = 0;
+	bool isOwned();
+};
+
+
+class StartField : public NotOwnedField {
+public:
+	StartField(const string& name, Money prize);
+
+	virtual effectPtr pass();
+	virtual effectPtr stop();
+
+private:
+	Money prize;
+};
+
+
+class AwardField : public NotOwnedField {
+public:
+	AwardField(const string& name, Money prize);
+
+	virtual effectPtr stop();
+
+private:
+	Money prize;
+};
+
+
+class PenaltyField : public NotOwnedField {
+public:
+	PenaltyField(const string& name, Money penalty);
+
+	virtual effectPtr stop();
+
+private:
+	Money penalty;
+};
+
+
+class DepositField : public NotOwnedField {
+public:
+	DepositField(const string& name, Money penalty);
+
+	virtual effectPtr pass();
+	virtual effectPtr stop();
+	virtual void store(Money amount);
+
+private:
+	Money penalty;
+	Money currentSum;
+};
+
+
+class AquariumField : public NotOwnedField {
+	AquariumField(const string& name, unsigned int rounds);
+
+	virtual effectPtr stop();
+
+private:
+	unsigned int rounds;
+};
+
+
+class PropertyField : public Field {
+public:
+	PropertyField(const string& name, const Money cost, const Money penalty);
+
+	virtual effectPtr stop() = 0;
+
+protected:
+	Money penalty;
+};
+
+
+class PublicPropertyField : public PropertyField {
+public:
+	PublicPropertyField(const string& name, const Money cost);
+
+	virtual effectPtr stop();
+};
+
+
+class CoralField : public PropertyField {
+public:
+	CoralField(const string& name, const Money cost);
+
+	virtual effectPtr stop();
+};
+
+class IslandField : public NotOwnedField {
+	public:
+		virtual effectPtr stop();
+};
+
+#endif
