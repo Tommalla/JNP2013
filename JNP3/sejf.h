d8175bd79691c5848e417c7f6b2ff8832664fded
Tomasz Zakrzewski <t.zakrzewski@student.uw.edu.pl>
2014-09-07 15:34:23 +0100

    Cleaned up JNP3.

--- JNP3/sejf.h
+++ JNP3/sejf.h
@@ -1,16 +1,42 @@
-/**
- * Tomasz Zakrzewski i Mateusz Bałasz
- * Trzecie zadanie z JNP
- */
 #ifndef SEJF_H
 #define SEJF_H
+
+#include <string>
 #include "kontroler.h"
 
 class Kontroler;
 
-class Sejf {
-	public:
-		const Kontroler kontroler();
+class Sejf final
+{
+public:
+	friend class Kontroler;
+
+	Sejf(const std::string& text, unsigned int numOfAttempts = 42)
+		: _text(text)
+		, _numOfAttempts(numOfAttempts)
+		, _status(Status::OK)
+		, _kontroler(&_status, &_numOfAttempts)
+		{
+		}
+	Sejf(Sejf&& other);
+
+public:
+	short int operator[](const int idx);
+	Sejf& operator+=(const unsigned int val);
+	Sejf& operator-=(const unsigned int val);
+	Sejf& operator*=(const unsigned int val);
+	Sejf& operator=(Sejf&& other);
+	const Kontroler& kontroler() const;
+
+private:
+	std::string _text;
+	unsigned int _numOfAttempts;
+	Status _status;
+	const Kontroler _kontroler;
+
+	void move(Sejf&& other);
+	void setAlert();
+	void setModified();
 };
 
-#endif
+#endif // SEJF_H
